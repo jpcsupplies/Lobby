237695a1d3e57797deb9751d54fc467c337f28c1
xPhoenixXx <jpcsupplies@hotmail.com>
2016-07-07 00:15:48 +1000

    Production Server Readiness.
    No longer hard coded.
    Hotspots are set by LCDs
    Various crazy stuff added
    Help updated
    command renamed

--- Data/Scripts/Lobby.scripts/Lobby.cs
+++ Data/Scripts/Lobby.scripts/Lobby.cs
@@ -85,7 +85,7 @@ namespace Economy.scripts
                 //Lets let the user know whats up.
                  MyAPIGateway.Utilities.ShowMessage("Lobby", "loaded!");
                  MyAPIGateway.Utilities.ShowMessage("Lobby", "Type '/Lhelp' for more informations about available commands\r\ntype /jump to go to current server");
-                 MyAPIGateway.Utilities.ShowMissionScreen("Lobby", "", "Warning", "Welcome to gateway Station.\r\n\r\nThis server has been decomissioned.\r\nEnter a shuttle and type /jump to travel to its sector.\r\nWatch hub for sector indicator.", null, "Close");
+                 MyAPIGateway.Utilities.ShowMissionScreen("Lobby", "", "Warning", "Welcome to gateway Station.\r\n\r\nPlease enter a shuttle and when indicated on hud..\r\nType /depart to travel to its sector.", null, "Close");
             }
         }
 
@@ -112,11 +112,11 @@ namespace Economy.scripts
                     counter = 0;
                     if (UpdateLobby())
                     {
-                        MyAPIGateway.Utilities.GetObjectiveLine().Objectives[0] = Zone + " Type /jump to depart on this shuttle";
+                        MyAPIGateway.Utilities.GetObjectiveLine().Objectives[0] = Zone + " [Type /depart to travel here]";
                         // uncomment this line if you want players to travel immediately on entering zone
                         //MyAPIGateway.Multiplayer.JoinServer(Target);
                     }
-                    else { MyAPIGateway.Utilities.GetObjectiveLine().Objectives[0] = "Scanning..."; }
+                    else { Zone = "Scanning..."; Target = "none"; MyAPIGateway.Utilities.GetObjectiveLine().Objectives[0] = Zone; }
                 }
                 counter++;
             } 
@@ -131,26 +131,72 @@ namespace Economy.scripts
 
         public bool UpdateLobby()
         {
-            //MyAPIGateway.Utilities.ShowMessage("Lobby", "Scanning..");
             //check our position - are we in a hot spot?
             //if (MyAPIGateway.Session.Player?.Controller?.ControlledEntity != null) {
-             if ( MyAPIGateway.Session.Player.Controller.ControlledEntity != null){
-                Vector3D position = MyAPIGateway.Session.Player.Controller.ControlledEntity.Entity.GetPosition();
-                double X = position.X; double Y = position.Y; double Z = position.Z;
-                string whereami = string.Format("[ X: {0:F0} Y: {1:F0} Z: {2:F0} ]", X, Y, Z);
-                //MyAPIGateway.Utilities.ShowMessage("debug", whereami);
-                MyAPIGateway.Utilities.GetObjectiveLine().Title = whereami;
+             if ( MyAPIGateway.Session.Player.Controller.ControlledEntity != null)
+             {
+                 Vector3D position = MyAPIGateway.Session.Player.Controller.ControlledEntity.Entity.GetPosition();
+                 double X = position.X; double Y = position.Y; double Z = position.Z;
+                 string whereami = string.Format("[ X: {0:F0} Y: {1:F0} Z: {2:F0} ]", X, Y, Z);
+                 MyAPIGateway.Utilities.GetObjectiveLine().Title = whereami;
 
-                //hard coded target list cause I suck at server side datafiles..
-                if (X >= -100 && X<=100 && Y >= -100 && Y<=100 && Z >= 15 && Z <=25) { Zone = "Lawless void"; Target = "221.121.159.238:27270"; return true; }
-                if (X >= -100 && X <= -10 && Y >= 10 && Y <= 100 && Z >= -20 && Z <= 11) { Zone = "Black Talon Sector"; Target = "59.167.215.81:27016"; return true; }
-                if (X >= -100 && X <= -10 && Y >= -100 && Y <= -10 && Z >= -20 && Z <= 11) { Zone = "Spokane Survivalist Sector"; Target = "162.248.94.205:27065"; return true; }
-                if (X >= 10 && X <= 100 && Y >= -100 && Y <= -10 && Z >= -20 && Z <= 11) { Zone = "Pandora Sector"; Target = "91.121.145.20:27016"; return true; }
-                if (X >= 10 && X <= 100 && Y >= 10 && Y <= 100 && Z >= -20 && Z <= 11) { Zone = "Ah The Final Frontier"; Target = "192.99.150.136:27039"; return true; }
-                else { Zone = "Scanning..."; Target = "none"; return false;  } 
+                 //hard coded target list cause I suck at server side datafiles..
+                 /* if (X >= -100 && X<=100 && Y >= -100 && Y<=100 && Z >= 15 && Z <=25) { Zone = "Lawless void"; Target = "221.121.159.238:27270"; return true; }
+                  if (X >= -100 && X <= -10 && Y >= 10 && Y <= 100 && Z >= -20 && Z <= 11) { Zone = "Black Talon Sector"; Target = "59.167.215.81:27016"; return true; }
+                  if (X >= -100 && X <= -10 && Y >= -100 && Y <= -10 && Z >= -20 && Z <= 11) { Zone = "Spokane Survivalist Sector"; Target = "162.248.94.205:27065"; return true; }
+                  if (X >= 10 && X <= 100 && Y >= -100 && Y <= -10 && Z >= -20 && Z <= 11) { Zone = "Pandora Sector"; Target = "91.121.145.20:27016"; return true; }
+                  if (X >= 10 && X <= 100 && Y >= 10 && Y <= 100 && Z >= -20 && Z <= 11) { Zone = "Ah The Final Frontier"; Target = "192.99.150.136:27039"; return true; }
+                  else { Zone = "Scanning..."; Target = "none"; return false;  }  */
 
-            } 
-            return false;  //we fell through a hole           
+                  
+                //rip of lcd server code off economy mod..  all it needs to do is test for a block name and any strings
+                //look for an lcd named [destination] then grab the public title and extract a server address and caption
+                //display the caption in hud, and set the server address to connect to
+
+                var players = new List<IMyPlayer>();
+                MyAPIGateway.Players.GetPlayers(players, p => p != null);
+                var updatelist = new HashSet<IMyTextPanel>();
+
+                foreach (var player in players)
+                {
+                    // Establish a visual range of the LCD.
+                    // if there are no players closer than this, don't bother updating it.
+                    var sphere = new BoundingSphereD(player.GetPosition(), 9);
+                    var list = MyAPIGateway.Entities.GetEntitiesInSphere(ref sphere);
+                    string[] LCDTags = new string[] { "[destination]", "(destination)" };
+                    foreach (var block in list)
+                    {
+                        var textPanel = block as IMyTextPanel;
+                        if (textPanel != null
+                            && textPanel.IsFunctional
+                            && textPanel.IsWorking
+                            && LCDTags.Any(tag => textPanel.CustomName.IndexOf(tag, StringComparison.InvariantCultureIgnoreCase) >= 0))
+                        {
+                            updatelist.Add((IMyTextPanel)block);
+                        }
+                    }
+                }
+
+                foreach (var textPanel in updatelist)
+                { 
+                    var checkArray = (textPanel.GetPublicTitle() + " " + textPanel.GetPrivateTitle()).Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                 
+                    if (checkArray.Length >= 2) //if its Not at least 2 its invalid.
+                    {
+                        int title = 0; Zone = "";
+                        foreach (var str in checkArray) {
+                                //string reply = "i got " + checkArray.Length + " " + checkArray[0] + " " + checkArray[1];
+                                //MyAPIGateway.Utilities.ShowMessage("TEST", reply);
+                                if (title != 0 && title <= checkArray.Length) { Zone += " " + checkArray[title]; } 
+                                title++;
+                        } Target = checkArray[0]; return true; //this should probably check if ip is valid format
+                    }
+                    else { return false; }           
+                }
+             }
+            
+            return false;
+            //we fell through a hole nothing to see here
         }
 
         #region command list
@@ -161,8 +207,8 @@ namespace Economy.scripts
             if (split.Length == 0)
                 return false;
 
-            #region jump
-            if (split[0].Equals("/jump", StringComparison.InvariantCultureIgnoreCase))
+            #region depart
+            if (split[0].Equals("/depart", StringComparison.InvariantCultureIgnoreCase) || split[0].Equals("/jump", StringComparison.InvariantCultureIgnoreCase))
             {
                 if (Zone != "Scanning..." && Target != "none")
                 {                
@@ -177,11 +223,57 @@ namespace Economy.scripts
             //ver reply
             if (split[0].Equals("/ver", StringComparison.InvariantCultureIgnoreCase))
             {
-                string versionreply = "2 " ;
+                string versionreply = "3 " ;
                 MyAPIGateway.Utilities.ShowMessage("VER", versionreply);
                 return true;
             }
             #endregion ver
+
+            #region test 
+            //This tests scan results and displays what the mod see's
+            if (split[0].Equals("/test", StringComparison.InvariantCultureIgnoreCase))
+            {
+            var players = new List<IMyPlayer>();
+            MyAPIGateway.Players.GetPlayers(players, p => p != null);
+            var updatelist = new HashSet<IMyTextPanel>();
+
+            foreach (var player in players)
+            {
+                var sphere = new BoundingSphereD(player.GetPosition(), 9);
+                var list = MyAPIGateway.Entities.GetEntitiesInSphere(ref sphere);
+                string[] LCDTags = new string[] { "[destination]", "(destination)" };
+                foreach (var block in list)
+                {
+                    
+                    var textPanel = block as IMyTextPanel;
+                    if (textPanel != null
+                        && textPanel.IsFunctional
+                        && textPanel.IsWorking
+                        && LCDTags.Any(tag => textPanel.CustomName.IndexOf(tag, StringComparison.InvariantCultureIgnoreCase) >= 0))
+                    {
+                        updatelist.Add((IMyTextPanel)block);
+                    }
+                }
+            }
+
+             foreach (var textPanel in updatelist)
+            { 
+                 var checkArray = (textPanel.GetPublicTitle() + " " + textPanel.GetPrivateTitle()).Split(new Char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                     string reply="";
+                     if (checkArray.Length >= 2) {
+                         int title = 0; Zone = "";
+                         foreach (var str in checkArray)
+                         {
+                             if (title != 0 && title <= checkArray.Length) { Zone += " " + checkArray[title]; }
+                             title++;
+                         } reply = "i got Length " + checkArray.Length + " " + checkArray[0] + " " + Zone;
+                     }  else { reply = " I got less than 2 - so it is invalid";  }
+                     MyAPIGateway.Utilities.ShowMessage("TEST", reply);
+
+           }
+            return true;
+            }
+            #endregion test
 
             #region help
             // help command
@@ -189,21 +281,31 @@ namespace Economy.scripts
             {
                 if (split.Length <= 1)
                 {
-                    MyAPIGateway.Utilities.ShowMessage("Lhelp", "Commands: Lhelp, jump, ver");
+                    MyAPIGateway.Utilities.ShowMessage("Lhelp", "Commands: Lhelp, depart, ver, test");
                     MyAPIGateway.Utilities.ShowMessage("Lhelp", "Try '/Lhelp command' for more informations about specific command");
                     return true;
                 }
                 else
                 {
-                    string helpreply = "."; //this reply is too big need to move it to pop up \r\n
+                    string helpreply = ""; 
                     switch (split[1].ToLowerInvariant())
                     {
-                        case "jump":
-                            helpreply = "transfers player to another server world\r\n" +
+                        case "depart":
+                            helpreply = "Player travels to another server world\r\n" +
                                 "The world you connect to depends on your location\r\n" +
-                                "by default it connects to the default world\r\n";
-                            MyAPIGateway.Utilities.ShowMessage("LHelp", "Example: /jump");
-                            MyAPIGateway.Utilities.ShowMissionScreen("lobby Help", "", "jump command", helpreply, null, "Close");
+                                "and if the server is online or not.\r\n";
+                            MyAPIGateway.Utilities.ShowMessage("LHelp", "Example: /depart");
+                            MyAPIGateway.Utilities.ShowMissionScreen("lobby Help", "", "depart command", helpreply, null, "Close");
+                            return true;
+                        case "ver":
+                            helpreply = "Simply shows an internal reference version number\r\n";
+                            MyAPIGateway.Utilities.ShowMessage("LHelp", "Example: /ver");
+                            MyAPIGateway.Utilities.ShowMissionScreen("lobby Help", "", "ver command", helpreply, null, "Close");
+                            return true;
+                        case "test":
+                            helpreply = "Simply shows what the mod scanner is picking up\r\n";
+                            MyAPIGateway.Utilities.ShowMessage("LHelp", "Example: /test");
+                            MyAPIGateway.Utilities.ShowMissionScreen("lobby Help", "", "test command", helpreply, null, "Close");
                             return true;
                     }
                 }
